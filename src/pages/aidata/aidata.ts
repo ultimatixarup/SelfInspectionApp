42d00e7eb8b9f0754dd9959d2e5533ca0f70ae15
ultimatixarup <arupkumar.banerjee@gmail.com>
2018-05-31 16:08:08 -0500

    update

--- src/pages/aidata/aidata.ts
+++ src/pages/aidata/aidata.ts
@@ -3,6 +3,7 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import { AppSettingsComponent } from '../../components/app-settings/app-settings';
 import { Http } from '@angular/http';
 import { LoadingController } from 'ionic-angular';
+import { InspectiondetailsPage } from '../inspectiondetails/inspectiondetails';
 /**
  * Generated class for the AidataPage page.
  *
@@ -19,15 +20,23 @@ export class AidataPage {
 item:any;
 aidata:any;
 probabilities:any;
+photoId:any;
 aicolors:any;
   constructor(public navCtrl: NavController, public navParams: NavParams,public http:Http, public loadingCtrl:LoadingController) {
   this.aicolors = ["#6FB2D6","#C5E0EE","#DDEDF6","#F0F7FB"];
     this.item = navParams.get('data');
+    if(this.item.defaultPhotoId){
+        this.photoId = this.item.defaultPhotoId;
+    } else {
+        this.photoId = this.item.publicId;
+    }
+    
+    
   let loader = this.loadingCtrl.create({
     content: "Loading..."
   });
   loader.present();
-    this.http.get(AppSettingsComponent.AI_SERVICE+'?imageId='+this.item.defaultPhotoId).subscribe(resp => {
+    this.http.get(AppSettingsComponent.AI_SERVICE+'?imageId='+this.photoId).subscribe(resp => {
                                      
         this.aidata = JSON.parse(resp['_body']);
         this.probabilities = this.aidata.probabilities;
@@ -61,5 +70,9 @@ aicolors:any;
     if(i==0) return "white";
     return "black";
   }
+  
+   gohome(){
+        this.navCtrl.setRoot(InspectiondetailsPage);
+      }
   
 }
