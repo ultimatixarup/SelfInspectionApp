4b0983d2e575f899bc0ae0d9c1e5795103c550e3
Arup <arup.banerjee@toyota.com>
2018-06-12 14:24:37 -0500

    update

--- src/pages/inspectiondetails/inspectiondetails.ts
+++ src/pages/inspectiondetails/inspectiondetails.ts
@@ -149,6 +149,7 @@ this.http.get(AppSettingsComponent.INSPECTION_RESOURCE_FINDING).subscribe(resp =
         item.vin.toLowerCase().indexOf(val.toLowerCase()) > -1 ||
         (item.licensePlateNumber+'').toLowerCase().indexOf(val.toLowerCase()) > -1 ||
         (item.licensePlateState+'').toLowerCase().indexOf(val.toLowerCase()) > -1 ||
+        (item.odometer+'').toLowerCase().indexOf(val.toLowerCase()) > -1 ||
         item.createDate.split('T')[0].indexOf(val.toLowerCase()) > -1
         );
       })
