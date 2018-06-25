b88c91b92283e867130c6d3d2352be62777a3a49
Arup <arup.banerjee@toyota.com>
2018-06-25 13:55:57 -0500

    update

--- src/components/app-settings/app-settings.ts
+++ src/components/app-settings/app-settings.ts
@@ -13,9 +13,9 @@ import { Component } from '@angular/core';
 export class AppSettingsComponent {
 
   text: string;
-  public static API_ENDPOINT='http://inspection-service-dev02.herokuapp.com';
+  public static API_ENDPOINT='http://inspector.autof.in';
   
-  public static MEDIA_ENDPOINT= 'https://media-service-dev02.herokuapp.com' + '/media';
+  public static MEDIA_ENDPOINT= AppSettingsComponent.API_ENDPOINT + '/media';
 
     public static INSPECTION_SERVICE= AppSettingsComponent.API_ENDPOINT + '/inspection';
   public static INSPECTION_RESOURCE_NOUN= AppSettingsComponent.API_ENDPOINT + '/inspection/resources/noun';
@@ -23,7 +23,7 @@ export class AppSettingsComponent {
   public static INSPECTION_RESOURCE_DAMAGE = AppSettingsComponent.API_ENDPOINT + '/inspection/resources/damage';
   public static INSPECTION_RESOURCE_FINDING = AppSettingsComponent.API_ENDPOINT + '/inspection/resources/finding';
   public static INSPECTION_FINDING = AppSettingsComponent.API_ENDPOINT + '/inspection/finding';
-  public static VIN_SEARCH = 'https://vehicle-service-stage.herokuapp.com' + '/vehicle';
+  public static VIN_SEARCH =AppSettingsComponent.API_ENDPOINT + '/vehicle';
   public static INSPECTION_PHOTO = AppSettingsComponent.API_ENDPOINT + '/inspection/photo';
   
   
